b251ab3219938191fc486813a15e49b31c020c5a
Dylan Morris <dylanrobertmorris@gmail.com>
2021-06-01 11:54:39 -0500

    build: products recommended to the authenticated user by other users are now being returned in a GET request to the /profile resource

--- bangazonapi/views/profile.py
+++ bangazonapi/views/profile.py
@@ -84,6 +84,7 @@ class Profile(ViewSet):
             
             current_user = Customer.objects.get(user=request.auth.user)
             current_user.recommends = Recommendation.objects.filter(recommender=current_user)
+            current_user.recommended = Recommendation.objects.filter(customer=current_user)
 
             serializer = ProfileSerializer(
                 current_user, many=False, context={'request': request})
@@ -363,6 +364,15 @@ class RecommenderSerializer(serializers.ModelSerializer):
         model = Recommendation
         fields = ('product', 'customer',)
 
+class RecommendedSerializer(serializers.ModelSerializer):
+    """JSON serializer for recommendations"""
+    recommender = CustomerSerializer()
+    product = ProfileProductSerializer()
+
+    class Meta:
+        model = Recommendation
+        fields = ('product', 'recommender',)
+
 
 class ProfileSerializer(serializers.ModelSerializer):
     """JSON serializer for customer profile
@@ -372,11 +382,12 @@ class ProfileSerializer(serializers.ModelSerializer):
     """
     user = UserSerializer(many=False)
     recommends = RecommenderSerializer(many=True)
+    recommended = RecommendedSerializer(many=True)
 
     class Meta:
         model = Customer
         fields = ('id', 'url', 'user', 'phone_number',
-                    'address', 'payment_types', 'recommends',)
+                    'address', 'payment_types', 'recommends', 'recommended')
         depth = 1
         
 
